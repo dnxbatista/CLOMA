0373e699a954ccbbbb02c92f6b17ac1d33aa804f
Daniel Albuquerque <106679918+dnxbatista@users.noreply.github.com>
2025-09-04 12:46:23 -0300

    Update

--- obj/Debug/net8.0/CLOMAProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/CLOMAProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CLOMAProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b24c15c9a60ddcdd363d608f8628281e5b1a2737")]
 [assembly: System.Reflection.AssemblyProductAttribute("CLOMAProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CLOMAProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
